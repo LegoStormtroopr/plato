96e5e26ecd6a047fcaec4ad96bdefd66aacc2583
Samuel Spencer <theodore.therone@gmail.com>
2018-01-29 02:59:07 +0000

    change setup?

--- python/mallard-questionnaire-registry/setup.py
+++ python/mallard-questionnaire-registry/setup.py
@@ -6,10 +6,11 @@ with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
 
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+version='0.1',
 
 setup(
     name='django-aristotle-mallard-questionnaire-registry',
-    version='0.1',
+    version=__version__,
     packages=find_packages(),
     include_package_data=True,
     license='BSD Licence',
